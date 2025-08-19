f6665672432fc7ab43d2b1454ea039eadb06befc
iriye <attorney@centralfloridaattorney.net>
2025-08-18 22:07:49 -0400

    clean up junk

--- zai_toolbag/gan/__init__.py
+++
@@ -1,4 +0,0 @@
-# zai_toolbag/gan/__init__.py
-from .cupy_gan import CupyGAN, get_next_model_filename
-from .combined_cupy_gan import CombinedCupyGAN
-from .utils import create_blue_circle_image, create_sample_image, create_blue_square_image, create_red_circle_image
